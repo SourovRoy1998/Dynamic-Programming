ada213364ce4eda01570f691401a16fa7390a164
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-11-15 23:19:08 +0530

    Update is-subsequence.cpp

--- is-subsequence.cpp
+++ is-subsequence.cpp
@@ -30,3 +30,25 @@ public:
         return memoize[m][n];
     }
 };
+
+
+
+//Efficient Solution
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        int i=0,j=0;
+        
+        while(i<m && j<n){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        
+        return i==m;
+        
+    }
+};
