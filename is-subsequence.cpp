3427ca529d15eb2d0e89342fc5fa496783b3335d
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-11-15 23:15:37 +0530

    Create is-subsequence.cpp

---
+++ is-subsequence.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/is-subsequence
+//Very inefficient code
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        
+        bool memoize[m+1][n+1];
+        
+        for(int i=0;i<=m;i++)
+            for(int j=0;j<=n;j++){
+                if(i==0)
+                    memoize[i][j]=true;
+                else if(j==0)
+                    memoize[i][j]=false;
+                else if(s[i-1]==t[j-1])
+                    memoize[i][j]=memoize[i-1][j-1];
+                else 
+                    memoize[i][j]=memoize[i][j-1];
+            }
+        
+       /* for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++)
+                cout<<memoize[i][j]<<" ";
+            cout<<endl;
+        }
+        */
+        return memoize[m][n];
+    }
+};
